ed89cf1f2dcacc93b2e3be77389cfeafcb5eb49a
misakano7545 <misakano7545@protonmail.com>
2022-06-29 21:37:15 +0800

    Add more API for display IP address

--- util/common/network.go
+++ util/common/network.go
@@ -9,6 +9,9 @@ func GetMyIpAddr() string {
 	resp, err := http.Get("https://api.ip.sb/ip")
 	if err != nil {
 		resp, _ = http.Get("https://api64.ipify.org")
+		if err != nil {
+			resp, _ = http.Get("http://ip.cip.cc")
+		}
 	}
 	defer resp.Body.Close()
 	s, _ := ioutil.ReadAll(resp.Body)
